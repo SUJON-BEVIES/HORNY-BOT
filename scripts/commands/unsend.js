f0bfce790519a7897703fe8f5ac203cb6fbb7ad0
Nisan || EditZ <mdnisanofficial@gmail.com>
2024-06-22 23:26:13 +0600

    Update unsend.js

--- scripts/commands/unsend.js
+++ scripts/commands/unsend.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-	name: "uns",
+	name: "uns", "😾"
 	version: "1.0.5",
 	permission: 2,
 	credits: "Nayan",
 	prefix: true,
-	description: "ban or unblock users",
-	category: "admin",
+	description: "ban or unblock users", "msg teact emuji 😾"
+	category: "react message 😾"
 	usages: " ",
 	cooldowns: 5
 };
@@ -18,12 +18,13 @@ module.exports.languages = {
 	},
 	"en": {
 		"returnCant": "Can't to unsend message from other user.",
-		"missingReply": "Reply to the message you want to unsend."
+		"missingReply": "Reply to the message you want to unsend.",
+		"missingmsg": "React to the message you want to unsend."
 	}
 }
 
 module.exports.run = function({ api, event, getText }) {
 	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
-	if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
-	return api.unsendMessage(event.messageReply.messageID);
+	if (event.type != "message_reply", "message_react") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
+	return api.unsendMessage(event.messageReply.messegeReact.messageID);
 	}
