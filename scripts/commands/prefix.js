cd58414884c27016bd7e05d9693d8647b68671e5
Nisan || EditZ <mdnisanofficial@gmail.com>
2024-07-12 21:57:57 +0600

    Update prefix.js

--- scripts/commands/prefix.js
+++ scripts/commands/prefix.js
@@ -25,9 +25,9 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
     if (body === i.toUpperCase() | body === i | str === body) {
 		const prefix = threadSetting.PREFIX || global.config.PREFIX;
       if (config.PREFIX == null) {
-        return out(`bot prefix : ${global.config.PREFIX}`)
+        return out(`my prefix 👉 ${global.config.PREFIX}`)
       }
-      else return out(`bot prefix : ${global.config.PREFIX}`)
+      else return out(`my prefix 👉 ${global.config.PREFIX}`)
     }
 
   });
