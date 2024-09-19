b6cf5ecdbbdd47b55d08f9deeca9f39bccdf4376
Sujon Be'vies <159610261+sujonbbz@users.noreply.github.com>
2024-09-19 14:51:13 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -45,6 +45,6 @@ Credit: Ariyan Ahmed
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : AMI GORIB VAI
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/61564055655700`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/61556196058001/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/61564055655700/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
