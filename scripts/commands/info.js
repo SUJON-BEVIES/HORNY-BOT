96c2c7e5112abf716864287a23b433db7a58abef
Ｚ１Ｎ３Ｘ  <sujoinnnaaa@gmail.com>
2024-09-20 06:26:45 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "admin",
     version: "1.0.0",
     permission: 0,
-    credits: "nayan",
+    credits: "horny",
     prefix: true,
     description: "",
     category: "prefix",
@@ -28,23 +28,23 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`আসসালামু আলাইকুম
 --------------------------------------------
-Credit: Ariyan Ahmed
+Credit: YOUR DAD ARIYAN💀
 
 
-𝐍𝐚𝐦𝐞       : HORNY MINHAZ
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : MINHAZ SIXNINE
+𝐍𝐚𝐦𝐞       : HORNY SARA🫦
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : SARA BB'E
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: TUI JAINA KI KORBI VAI?🙂
 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: TMR BASHAR PICHE
-𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
+𝐆𝐞𝐧𝐝𝐞𝐫.   : FEMALE
 𝐀𝐠𝐞           : TOR BORO AMI😾
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : PANIR MOTO PURE SINGLE😼
 𝐖𝐨𝐫𝐤        : HORNY MOOD🥵
 𝐆𝐦𝐚𝐢𝐥       : NAI VAI KHOMA🙂🙏
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: AGE I LOVE YOU KO😾👍
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : AMI GORIB VAI
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/61564055655700`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : লিংক দিম্না😿`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/61564055655700/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://imgur.com/a/8g96rmA`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
